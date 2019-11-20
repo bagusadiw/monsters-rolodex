b7cbd1b32182b8bd4dda1a25718afed3e1a9c3bb
shiroyawn <bagusadi1494@gmail.com>
2019-11-20 13:16:58 +0700

    Updates

---
+++ precache-manifest.181ad1c724eb2fbce9b80bd8d4621464.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbea23f00b31f76c45e1e7e3d239f96",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "435cddedb9a6c0b1a7c7",
+    "url": "/monsters-rolodex/static/css/main.f124edbb.chunk.css"
+  },
+  {
+    "revision": "5bc7c05f66c3ef831429",
+    "url": "/monsters-rolodex/static/js/2.df32befe.chunk.js"
+  },
+  {
+    "revision": "435cddedb9a6c0b1a7c7",
+    "url": "/monsters-rolodex/static/js/main.a56d00a1.chunk.js"
+  },
+  {
+    "revision": "075343449241dbe0ad6c",
+    "url": "/monsters-rolodex/static/js/runtime-main.58539e12.js"
+  }
+]);
